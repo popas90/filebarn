783c549c92a379f9cb8123a55f79ccc9748652e4
popas90 <bogdititupopa@gmail.com>
2016-04-18 11:31:41 +0300

    prepare for db creation

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'h@rd-t0-gue55-k3y'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbdir = os.path.join(basedir, 'db')
+
+# TODO use the debug deatabase for now
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dbdir, 'debug.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(dbdir, 'repository')
